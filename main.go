eacfa116a2d221c0371acca732e5e606f707b98a
Adrian Moya <adrianmoya@gmail.com>
2015-09-05 19:33:56 -0500

    Godeps

--- main.go
+++ main.go
@@ -1,28 +1,27 @@
-package main 
+package main
 
 import (
-	"net/http"
+	"adrianmoya.com/go-web-hello/Godeps/_workspace/src/github.com/gorilla/mux"
 	"fmt"
-	"os"
 	"log"
-	"github.com/gorilla/mux"
+	"net/http"
+	"os"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "8080"		
+		port = "8080"
 	}
 
-	log.Printf("Using port %s", port) 
+	log.Printf("Using port %s", port)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
-	http.ListenAndServe(":"+port,r)
+	http.ListenAndServe(":"+port, r)
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love Go (or what?)!")
 }
-
